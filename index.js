ac0dcc743ff13cc082887253787cac5aa2c0ed72
sujay000 <sujaysapkal22@gmail.com>
2024-05-31 18:13:38 +0530

    added styles

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ const init = () => {
 init()
 
 const handleRememberMe = () => {
-    console.log('handleRememberMe called')
     const rememberMeBox = document.getElementsByClassName('rememberMe')[0]
     const address = rememberMeBox.value || ''
 
@@ -24,29 +23,85 @@ const handleUseMyAddress = () => {
     addressBox.value = localStorage.getItem(SOL_FAUCET_BY_SUJAY) || ''
 }
 
+const handleResult = ({ success, result }) => {
+    removeLoading()
+
+    const msgBox = document.getElementsByClassName('msg')[0]
+
+    const msgContent = document.getElementsByClassName('msg-content')[0]
+    msgContent.innerHTML = result
+
+    msgBox.classList.add(['active'])
+    if (success) {
+        msgBox.classList.add(['success'])
+    } else {
+        msgBox.classList.add(['danger'])
+    }
+}
+
 // const solanaWeb3 = require('@solana/web3.js') // already comes from the script src
 const handleGetSol = async () => {
+    hideThis()
+    addLoading()
+
     const address = document.getElementsByClassName('addressInput')[0].value
     const solValueString = document.getElementById('solValue').value
     const solValue = Number(solValueString)
 
     if (address === '') {
-        console.log('address cannot be empty')
+        handleResult({
+            success: false,
+            result: 'Wallet address cannot be empty',
+        })
         return
     }
     if (!(solValue >= 0 && solValue <= 2)) {
-        console.log('SOL value should be greater than 0 and less than 2')
+        handleResult({
+            success: false,
+            result: 'SOL value should be greater than 0 and less than 2',
+        })
+
         return
     }
+
     console.log(solanaWeb3)
     const { Connection, clusterApiUrl, PublicKey, LAMPORTS_PER_SOL } = solanaWeb3
     const connnection = new Connection(clusterApiUrl('devnet'))
 
     try {
         console.log(`Requesting ${solValue} SOL airdrop for ${address}`)
-        const signature = await connnection.requestAirdrop(new PublicKey(address), solValue)
-        console.log(`Transaction: https://explorer.solana.com/tx/${signature}?cluster=devnet`)
+        const signature = await connnection.requestAirdrop(new PublicKey(address), solValue * LAMPORTS_PER_SOL)
+
+        handleResult({
+            success: true,
+            result: `<a href="https://explorer.solana.com/tx/${signature}?cluster=devnet">Transaction Link</a>`,
+        })
     } catch (e) {
-        console.log(e)
+        handleResult({
+            success: false,
+            result: String(e),
+        })
     }
+}
+
+const hideThis = () => {
+    const msgBox = document.getElementsByClassName('msg')[0]
+    msgBox.classList.remove(['active'])
+    msgBox.classList.remove(['success'])
+    msgBox.classList.remove(['danger'])
+}
+
+const addLoading = () => {
+    const msgBox = document.getElementsByClassName('msg')[0]
+    msgBox.classList.add(['active'])
+    msgBox.classList.add(['loading'])
+
+    const msgContent = document.getElementsByClassName('msg-content')[0]
+    msgContent.innerHTML = 'Please wait...'
+}
+
+const removeLoading = () => {
+    const msgBox = document.getElementsByClassName('msg')[0]
+    msgBox.classList.remove(['active'])
+    msgBox.classList.remove(['loading'])
 }
